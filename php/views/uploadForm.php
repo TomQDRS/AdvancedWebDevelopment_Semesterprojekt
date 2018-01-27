88307a0aff4239349da23cf24d10a79057cefcd2
DESKTOP-SV1MC5G\Tom Quinders <tomquinders@gmail.com>
2018-01-27 23:16:49 +0100

    upload form styled aswell

--- php/views/uploadForm.php
+++ php/views/uploadForm.php
@@ -3,54 +3,63 @@
 
 <head>
     <title>toomanyimages</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../../css/style.css">
 </head>
 
 <body>
     <nav>
-        <img src="logos/imgup.png" alt="imgup logo" height="36" width="128" id="logo_nav_img" onclick="document.location.href='index.php'">
+        <img src="../../logos/tmi_logo_text.png" alt="toomanyimages logo" height="36" width="101" id="logo_nav_img" onclick="document.location.href='../../index.php'">
     </nav>
 
-    <section id="uploadContainer">
-        
-        <form id="imageUploadForm" action = "imageUpload.php" method="post" enctype="multipart/form-data">
-            
-            <input type="file" id="imageToUpload" name="imageToUpload" style="display: none;"/>
-            
-            <div id="uploadButtonContainerField">
-                <button type="button" id="upload_image_button" onclick="document.getElementById('imageToUpload').click();">Hochladen</button>
-            </div>
-            <br>Name
-            <input name="img_name" type="text" placeholder="Name" required> <br><br>Beschreibung
-            <input name="img_desc" type="text" placeholder="Beschreibung"> <br><br>Bild privat hochladen
-            <input name="img_private" type="checkbox" value="private"><br><br><!--Tag
+    <section id="formcontainer">
+
+        <form id="imageUploadForm" action="../control/imageUpload.php" method="post" enctype="multipart/form-data" onsubmit="return validateForm();">
+
+            <input class type="file" id="imageToUpload" name="imageToUpload" style="display: none;"/>
+
+                <button type="button" class="formsubmit" id="upload_image_button" onclick="document.getElementById('imageToUpload').click();">Datei Auswählen</button>
+            <input class="formtextinput" name="img_name" type="text" placeholder="Titel" required>Titel
+            <input class="formtextinput" name="img_desc" type="text" placeholder="Beschreibung">Beschreibung
+            <div class="formcheckboxcontainer"><input class="formcheckbox" name="img_private" type="checkbox" value="private">Bild privat hochladen</div>
+            <!--Tag
             <input id="img_tag_input"  type="text" placeholder="Tag suchen oder erstellen..."><br><br>
             <div id="tagcontainer">
             
             </div>
             <br><br>-->
-            
-            <input type="submit" name="submit" value="submit">
+
+            <input class="formsubmit" type="submit" name="submit" value="Hochladen">
         </form>
     </section>
 </body>
-    
+
 
 <script type="text/javascript">
-/*
+    function validateForm() {
+        var inp = document.getElementById('imageToUpload');
+        if (inp.files.length === 0) {
+            alert("Bitte wähle eine Bilddatei zum Hochladen.");
+            return false;
+        } else {
+            return true;
+        }
+    }
 
-    onkeyup="onkeyup_check(event)"
-
-    function onkeyup_check(e) { 
-
-     var keyCode = e.keyCode || e.which; 
-     if(keyCode == 13) { 
-         e.preventDefault(); 
-         alert("nibba"); 
-         return false; 
-    } 
-    }*/
-  
-</script>
     
+    /*
+
+        onkeyup="onkeyup_check(event)"
+
+        function onkeyup_check(e) { 
+
+         var keyCode = e.keyCode || e.which; 
+         if(keyCode == 13) { 
+             e.preventDefault(); 
+             alert("nibba"); 
+             return false; 
+        } 
+        }*/
+
+</script>
+
 </html>
